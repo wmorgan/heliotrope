cb4595b980fb309aa20203e58cc9c1017c4df60d
William Morgan <wmorgan-github@masanjin.net>
2011-02-26 23:21:48 -0800

    allow multiple maildirs with heliotrope-add
    Sorts all the files. This is the way to backfill multiple folders
    (e.g. inbox and sent) and have the times correct.

--- lib/heliotrope/maildir-walker.rb
+++ lib/heliotrope/maildir-walker.rb
@@ -1,17 +1,31 @@
 module Heliotrope
 class MaildirWalker
-  def initialize dir
-    @dir = dir
-    @files = (Dir[File.join(@dir, "cur", "*")] + Dir[File.join(@dir, "new", "*")]).sort
-    puts "; found #{@files.size} messages"
+  def initialize(*dirs)
+    @dirs = dirs
+    @files = nil
   end
 
   def next_message
+    @files ||= get_files
     return nil if @files.empty?
     IO.read(@files.shift)
   end
 
-  def done?; @files.empty? end
+  def done?
+    @files ||= get_files
+    @files.empty?
+  end
+
   def finish!; end
+
+private
+
+  def get_files
+    puts "; scanning #{@dirs.size} directories..."
+    dirs = @dirs.map { |d| d.gsub(/([\*\?\[\]])/, '\\\\\1') } # have to escape for globbing
+    files = dirs.map { |dir| Dir[File.join(dir, "cur", "*")] + Dir[File.join(dir, "new", "*")] }.flatten.sort
+    puts "; found #{files.size} messages"
+    files
+  end
 end
 end
