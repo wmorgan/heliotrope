c40333304b717fe4778295d553c1a4109f042c7d
William Morgan <wmorgan-github@masanjin.net>
2011-03-02 20:09:35 -0800

    make tests work again

--- test/test_heliotrope.rb
+++ test/test_heliotrope.rb
@@ -24,6 +24,7 @@ class HeliotropeTest < ::Test::Unit::TestCase
         :subject => "test message",
         :date => Time.now,
         :indexable_text => "i love mice",
+        :snippet => "i love mice",
         :refs => []
       }.merge opts
 
@@ -39,155 +40,155 @@ class HeliotropeTest < ::Test::Unit::TestCase
   def setup
     FileUtils.rm_rf TEST_DIR
     FileUtils.mkdir TEST_DIR
-    @store = Store.new TEST_DIR
+    @index = Index.new TEST_DIR
   end
 
   def teardown
-    @store.close
+    @index.close
     FileUtils.rm_rf TEST_DIR
   end
 
   def test_size
-    assert_equal 0, @store.size
+    assert_equal 0, @index.size
 
     m1 = MockMessage.new
-    x = @store.add_message m1
-    assert_equal 1, @store.size
+    x = @index.add_message m1
+    assert_equal 1, @index.size
 
     m2 = MockMessage.new
-    @store.add_message m2
-    assert_equal 2, @store.size
+    @index.add_message m2
+    assert_equal 2, @index.size
   end
 
   def test_adding_duplicate_messages_does_nothing
     m1 = MockMessage.new :msgid => "a"
-    @store.add_message m1
-    assert_equal 1, @store.size
+    @index.add_message m1
+    assert_equal 1, @index.size
 
     m2 = MockMessage.new :msgid => "a"
-    @store.add_message m2
-    assert_equal 1, @store.size
+    @index.add_message m2
+    assert_equal 1, @index.size
   end
 
   def test_added_messages_are_available_in_search
-    @store.set_query Query.new("body", "hello")
-    results = @store.get_some_results 100
+    @index.set_query Query.new("body", "hello")
+    results = @index.get_some_results 100
     assert_equal 0, results.size
 
     m1 = MockMessage.new :indexable_text => "hello bob"
-    docid, threadid = @store.add_message m1
+    docid, threadid = @index.add_message m1
 
-    @store.reset_query!
-    results = @store.get_some_results 100
+    @index.reset_query!
+    results = @index.get_some_results 100
     assert_equal 1, results.size
     assert_equal threadid, results.first[:thread_id]
   end
 
   def test_added_message_state_is_preserved
     m1 = MockMessage.new
-    docid, threadid = @store.add_message m1, %w(unread), []
+    docid, threadid = @index.add_message m1, %w(unread), []
 
-    summary = @store.load_messageinfo docid
+    summary = @index.load_messageinfo docid
     assert_equal Set.new(%w(unread)), summary[:state]
   end
 
   def test_added_message_state_is_searchable_via_labels
-    @store.set_query Query.new("body", "~unread")
-    assert_equal 0, @store.count_results
+    @index.set_query Query.new("body", "~unread")
+    assert_equal 0, @index.count_results
 
     m1 = MockMessage.new
-    docid, threadid = @store.add_message m1, %w(unread), []
+    docid, threadid = @index.add_message m1, %w(unread), []
 
-    assert_equal 1, @store.count_results
+    assert_equal 1, @index.count_results
   end
 
   def test_message_state_is_modifiable
     m1 = MockMessage.new
-    docid, threadid = @store.add_message m1
-    assert_equal Set.new, @store.load_messageinfo(docid)[:state]
+    docid, threadid = @index.add_message m1
+    assert_equal Set.new, @index.load_messageinfo(docid)[:state]
 
-    @store.update_message_state docid, %w(unread)
-    assert_equal Set.new(%w(unread)), @store.load_messageinfo(docid)[:state]
+    @index.update_message_state docid, %w(unread)
+    assert_equal Set.new(%w(unread)), @index.load_messageinfo(docid)[:state]
 
-    @store.update_message_state docid, %w(starred)
-    assert_equal Set.new(%w(starred)), @store.load_messageinfo(docid)[:state]
+    @index.update_message_state docid, %w(starred)
+    assert_equal Set.new(%w(starred)), @index.load_messageinfo(docid)[:state]
 
-    @store.update_message_state docid, %w(unread deleted)
-    assert_equal Set.new(%w(unread deleted)), @store.load_messageinfo(docid)[:state]
+    @index.update_message_state docid, %w(unread deleted)
+    assert_equal Set.new(%w(unread deleted)), @index.load_messageinfo(docid)[:state]
   end
 
   def test_message_state_ignores_random_stuff
     m1 = MockMessage.new
-    docid, threadid = @store.add_message m1, %w(hello there bob inbox unread is nice), []
-    assert_equal Set.new(%w(unread)), @store.load_messageinfo(docid)[:state]
+    docid, threadid = @index.add_message m1, %w(hello there bob inbox unread is nice), []
+    assert_equal Set.new(%w(unread)), @index.load_messageinfo(docid)[:state]
   end
 
   def test_added_thread_labels_are_applied_to_the_whole_thread
     m1 = MockMessage.new
-    docid, threadid = @store.add_message m1, [], %w(tired hungry)
+    docid, threadid = @index.add_message m1, [], %w(tired hungry)
 
-    summary = @store.load_threadinfo threadid
+    summary = @index.load_threadinfo threadid
     assert_equal Set.new(%w(tired hungry)), summary[:labels]
   end
 
   def test_thread_labels_are_available_in_search
     m1 = MockMessage.new :indexable_text => "hello bob"
-    docid, threadid = @store.add_message m1
+    docid, threadid = @index.add_message m1
 
-    @store.set_query Query.new("body", "~tired")
-    assert_equal 0, @store.count_results
+    @index.set_query Query.new("body", "~tired")
+    assert_equal 0, @index.count_results
 
-    @store.update_thread_labels threadid, %w(tired)
-    assert_equal 1, @store.count_results
+    @index.update_thread_labels threadid, %w(tired)
+    assert_equal 1, @index.count_results
 
-    results = @store.get_some_results 100
+    results = @index.get_some_results 100
     assert_equal threadid, results.first[:thread_id]
   end
 
   def test_thread_labels_from_added_messages_are_available_in_search
-    @store.set_query Query.new("body", "~tired")
-    results = @store.get_some_results 100
+    @index.set_query Query.new("body", "~tired")
+    results = @index.get_some_results 100
     assert_equal 0, results.size
 
     m1 = MockMessage.new :indexable_text => "hello bob"
-    docid, threadid = @store.add_message m1, [], %w(tired hungry)
+    docid, threadid = @index.add_message m1, [], %w(tired hungry)
 
-    @store.reset_query!
-    results = @store.get_some_results 100
+    @index.reset_query!
+    results = @index.get_some_results 100
     assert_equal 1, results.size
  end
 
  def test_thread_labels_are_modifiable
     m1 = MockMessage.new :indexable_text => "hello bob"
-    docid, threadid = @store.add_message m1
-    assert_equal Set.new, @store.load_threadinfo(threadid)[:labels]
+    docid, threadid = @index.add_message m1
+    assert_equal Set.new, @index.load_threadinfo(threadid)[:labels]
 
-    @store.update_thread_labels threadid, %w(hungry)
-    assert_equal Set.new(%w(hungry)), @store.load_threadinfo(threadid)[:labels]
+    @index.update_thread_labels threadid, %w(hungry)
+    assert_equal Set.new(%w(hungry)), @index.load_threadinfo(threadid)[:labels]
 
-    @store.update_thread_labels threadid, %w(tired)
-    assert_equal Set.new(%w(tired)), @store.load_threadinfo(threadid)[:labels]
+    @index.update_thread_labels threadid, %w(tired)
+    assert_equal Set.new(%w(tired)), @index.load_threadinfo(threadid)[:labels]
 
-    @store.update_thread_labels threadid, %w(hungry tired)
-    assert_equal Set.new(%w(hungry tired)), @store.load_threadinfo(threadid)[:labels]
+    @index.update_thread_labels threadid, %w(hungry tired)
+    assert_equal Set.new(%w(hungry tired)), @index.load_threadinfo(threadid)[:labels]
  end
 
  def test_messages_are_threaded
     m1 = MockMessage.new :msgid => "1"
-    docid1, threadid1 = @store.add_message m1
+    docid1, threadid1 = @index.add_message m1
 
     m2 = MockMessage.new :msgid => "2", :refs => ["1"]
-    docid2, threadid2 = @store.add_message m2
+    docid2, threadid2 = @index.add_message m2
 
     assert_equal threadid1, threadid2
 
     m3 = MockMessage.new :msgid => "3", :refs => ["2"]
-    docid3, threadid3 = @store.add_message m3
+    docid3, threadid3 = @index.add_message m3
 
     assert_equal threadid2, threadid3
 
     m4 = MockMessage.new :msgid => "4", :refs => ["1"]
-    docid4, threadid4 = @store.add_message m4
+    docid4, threadid4 = @index.add_message m4
 
     assert_equal threadid3, threadid4
  end
@@ -197,27 +198,27 @@ class HeliotropeTest < ::Test::Unit::TestCase
     m2 = MockMessage.new :msgid => "2", :refs => ["1"]
     m3 = MockMessage.new :msgid => "3", :refs => ["2"]
 
-    docid1, threadid1 = @store.add_message m1, %w(unread)
-    docid2, threadid2 = @store.add_message m2, %w(unread)
-    docid3, threadid3 = @store.add_message m3, %w(unread)
+    docid1, threadid1 = @index.add_message m1, %w(unread)
+    docid2, threadid2 = @index.add_message m2, %w(unread)
+    docid3, threadid3 = @index.add_message m3, %w(unread)
 
     assert_equal threadid1, threadid2
     assert_equal threadid2, threadid3
 
-    assert_equal Set.new(%w(unread)), @store.load_threadinfo(threadid1)[:state]
+    assert_equal Set.new(%w(unread)), @index.load_threadinfo(threadid1)[:state]
 
-    @store.update_message_state docid1, []
-    assert_equal Set.new(%w(unread)), @store.load_threadinfo(threadid1)[:state]
+    @index.update_message_state docid1, []
+    assert_equal Set.new(%w(unread)), @index.load_threadinfo(threadid1)[:state]
 
-    @store.update_message_state docid2, []
-    assert_equal Set.new(%w(unread)), @store.load_threadinfo(threadid1)[:state]
+    @index.update_message_state docid2, []
+    assert_equal Set.new(%w(unread)), @index.load_threadinfo(threadid1)[:state]
 
-    @store.update_message_state docid3, []
-    assert_equal Set.new, @store.load_threadinfo(threadid1)[:state]
+    @index.update_message_state docid3, []
+    assert_equal Set.new, @index.load_threadinfo(threadid1)[:state]
 
     ## now add some back
-    @store.update_message_state docid3, %w(starred)
-    assert_equal Set.new(%w(starred)), @store.load_threadinfo(threadid1)[:state]
+    @index.update_message_state docid3, %w(starred)
+    assert_equal Set.new(%w(starred)), @index.load_threadinfo(threadid1)[:state]
   end
 
   ## this captures a bug i had
@@ -226,60 +227,60 @@ class HeliotropeTest < ::Test::Unit::TestCase
     m2 = MockMessage.new :msgid => "2", :refs => ["1"]
     m3 = MockMessage.new :msgid => "3", :refs => ["2"]
 
-    docid1, threadid1 = @store.add_message m1
-    docid2, threadid2 = @store.add_message m2
-    docid3, threadid3 = @store.add_message m3
+    docid1, threadid1 = @index.add_message m1
+    docid2, threadid2 = @index.add_message m2
+    docid3, threadid3 = @index.add_message m3
 
     assert_equal threadid1, threadid2
     assert_equal threadid2, threadid3
 
-    assert_equal Set.new(%w(attachment)), @store.load_threadinfo(threadid1)[:state]
+    assert_equal Set.new(%w(attachment)), @index.load_threadinfo(threadid1)[:state]
   end
 
   def test_message_state_is_propagated_to_thread_as_a_disjunction_in_search
-    @store.set_query Query.new("body", "~unread")
-    assert_equal 0, @store.count_results
+    @index.set_query Query.new("body", "~unread")
+    assert_equal 0, @index.count_results
 
     m1 = MockMessage.new :msgid => "1"
     m2 = MockMessage.new :msgid => "2", :refs => ["1"]
     m3 = MockMessage.new :msgid => "3", :refs => ["2"]
 
-    docid1, threadid1 = @store.add_message m1, %w(unread)
-    docid2, threadid2 = @store.add_message m2, %w(unread)
-    docid3, threadid3 = @store.add_message m3, %w(unread)
+    docid1, threadid1 = @index.add_message m1, %w(unread)
+    docid2, threadid2 = @index.add_message m2, %w(unread)
+    docid3, threadid3 = @index.add_message m3, %w(unread)
 
     assert_equal threadid1, threadid2
     assert_equal threadid2, threadid3
 
-    assert_equal 1, @store.count_results
-    @store.update_message_state docid1, []
-    assert_equal 1, @store.count_results
-    @store.update_message_state docid2, []
-    assert_equal 1, @store.count_results
-    @store.update_message_state docid3, []
-    assert_equal 0, @store.count_results
+    assert_equal 1, @index.count_results
+    @index.update_message_state docid1, []
+    assert_equal 1, @index.count_results
+    @index.update_message_state docid2, []
+    assert_equal 1, @index.count_results
+    @index.update_message_state docid3, []
+    assert_equal 0, @index.count_results
 
-    @store.set_query Query.new("body", "~starred")
-    assert_equal 0, @store.count_results
-    @store.update_message_state docid3, %w(starred)
-    assert_equal 1, @store.count_results
+    @index.set_query Query.new("body", "~starred")
+    assert_equal 0, @index.count_results
+    @index.update_message_state docid3, %w(starred)
+    assert_equal 1, @index.count_results
   end
 
   def test_adding_messages_can_join_threads
     m1 = MockMessage.new :msgid => "1"
     m3 = MockMessage.new :msgid => "3", :refs => ["2"]
 
-    docid1, threadid1 = @store.add_message m1
-    docid3, threadid3 = @store.add_message m3
+    docid1, threadid1 = @index.add_message m1
+    docid3, threadid3 = @index.add_message m3
 
     assert_not_equal threadid1, threadid3
 
     m2 = MockMessage.new :msgid => "2", :refs => ["1"]
-    docid2, threadid2 = @store.add_message m2
+    docid2, threadid2 = @index.add_message m2
 
-    threadid1 = @store.load_messageinfo(docid1)[:thread_id]
-    threadid2 = @store.load_messageinfo(docid2)[:thread_id]
-    threadid3 = @store.load_messageinfo(docid3)[:thread_id]
+    threadid1 = @index.load_messageinfo(docid1)[:thread_id]
+    threadid2 = @index.load_messageinfo(docid2)[:thread_id]
+    threadid3 = @index.load_messageinfo(docid3)[:thread_id]
 
     assert_not_nil threadid1
     assert_equal threadid1, threadid2
@@ -291,16 +292,16 @@ class HeliotropeTest < ::Test::Unit::TestCase
     m2 = MockMessage.new :msgid => "2", :refs => ["1"]
     m3 = MockMessage.new :msgid => "3", :refs => ["2"]
 
-    docid1, threadid1 = @store.add_message m1
-    docid2, threadid2 = @store.add_message m2
+    docid1, threadid1 = @index.add_message m1
+    docid2, threadid2 = @index.add_message m2
 
-    @store.set_query Query.new("body", "~hungry")
-    assert_equal 0, @store.count_results
+    @index.set_query Query.new("body", "~hungry")
+    assert_equal 0, @index.count_results
 
-    docid3, threadid3 = @store.add_message m3, [], %w(hungry)
-    assert_equal 1, @store.count_results
+    docid3, threadid3 = @index.add_message m3, [], %w(hungry)
+    assert_equal 1, @index.count_results
 
-    results = @store.get_some_results 100
+    results = @index.get_some_results 100
     assert_equal threadid3, results.first[:thread_id]
     docids = results.first[:structure].flatten
     assert_includes docid1, docids
@@ -312,39 +313,39 @@ class HeliotropeTest < ::Test::Unit::TestCase
     m1 = MockMessage.new :msgid => "1"
     m3 = MockMessage.new :msgid => "3", :refs => ["2"]
 
-    docid1, threadid1 = @store.add_message m1, [], %w(fluffy)
-    assert_equal Set.new(%w(fluffy)), @store.load_threadinfo(threadid1)[:labels]
+    docid1, threadid1 = @index.add_message m1, [], %w(fluffy)
+    assert_equal Set.new(%w(fluffy)), @index.load_threadinfo(threadid1)[:labels]
 
-    docid3, threadid3 = @store.add_message m3, [], %w(bunny)
-    assert_equal Set.new(%w(bunny)), @store.load_threadinfo(threadid3)[:labels]
+    docid3, threadid3 = @index.add_message m3, [], %w(bunny)
+    assert_equal Set.new(%w(bunny)), @index.load_threadinfo(threadid3)[:labels]
 
     m2 = MockMessage.new :msgid => "2", :refs => ["1"]
-    docid2, threadid2 = @store.add_message m2
+    docid2, threadid2 = @index.add_message m2
 
-    assert_equal threadid2, @store.load_messageinfo(docid1)[:thread_id]
-    assert_equal threadid2, @store.load_messageinfo(docid2)[:thread_id]
-    assert_equal threadid2, @store.load_messageinfo(docid3)[:thread_id]
+    assert_equal threadid2, @index.load_messageinfo(docid1)[:thread_id]
+    assert_equal threadid2, @index.load_messageinfo(docid2)[:thread_id]
+    assert_equal threadid2, @index.load_messageinfo(docid3)[:thread_id]
 
-    assert_equal Set.new(%w(fluffy bunny)), @store.load_threadinfo(threadid2)[:labels]
+    assert_equal Set.new(%w(fluffy bunny)), @index.load_threadinfo(threadid2)[:labels]
   end
 
   def test_adding_messages_can_join_threads_and_their_labels_are_unionized_and_that_is_reflected_in_search
     m1 = MockMessage.new :msgid => "1"
     m3 = MockMessage.new :msgid => "3", :refs => ["2"]
 
-    docid1, threadid1 = @store.add_message m1, [], %w(fluffy)
-    docid3, threadid3 = @store.add_message m3, [], %w(bunny)
+    docid1, threadid1 = @index.add_message m1, [], %w(fluffy)
+    docid3, threadid3 = @index.add_message m3, [], %w(bunny)
 
-    @store.set_query Query.new("body", "~fluffy")
-    results = @store.get_some_results 100
+    @index.set_query Query.new("body", "~fluffy")
+    results = @index.get_some_results 100
     assert_equal 1, results.size
     assert_equal threadid1, results.first[:thread_id]
 
     m2 = MockMessage.new :msgid => "2", :refs => ["1"]
-    docid2, threadid2 = @store.add_message m2
+    docid2, threadid2 = @index.add_message m2
 
-    @store.reset_query!
-    results = @store.get_some_results 100
+    @index.reset_query!
+    results = @index.get_some_results 100
     assert_equal 1, results.size
 
     assert_equal threadid2, results.first[:thread_id]
