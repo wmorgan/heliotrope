a2b7887594330bfd641851cfaaa64bfae1dd33e0
William Morgan <wmorgan-github@masanjin.net>
2011-04-01 23:47:27 -0700

    add new methods to client as well, and refactor get and post code

--- lib/heliotrope-client.rb
+++ lib/heliotrope-client.rb
@@ -8,21 +8,16 @@ class HeliotropeClient
   end
 
   def search query, num=20, start=0
-    response = @resource["search.json"].get :params => { :q => query, :start => start, :num => num }
-    response.force_encoding Encoding::UTF_8 if in_ruby19_hell?
-    JSON.parse response
+    get_json "search", :q => query, :start => start, :num => num
   end
 
-  def thread id
-    response = @resource["thread/#{id}.json"].get
-    response.force_encoding Encoding::UTF_8 if in_ruby19_hell?
-    JSON.parse response
-  end
+  def thread id; get_json("thread/#{id}") end
+
+  def thread_labels id; get_json("thread/#{id}/labels") end
+  def thread_state id; get_json("thread/#{id}/state") end
 
   def message id, preferred_mime_type="text/plain"
-    response = @resource["message/#{id}.json"].get :params => { :mime_type_pref => preferred_mime_type }
-    response.force_encoding Encoding::UTF_8 if in_ruby19_hell?
-    JSON.parse response
+    get_json "message/#{id}", :mime_type_pref => preferred_mime_type
   end
 
   def message_part message_id, part_id
@@ -30,19 +25,32 @@ class HeliotropeClient
     @resource["message/#{message_id}/part/#{part_id}"].get
   end
 
-  def labels
-    JSON.parse @resource["labels"].get
-  end
+  def labels; get_json("labels") end
 
-  def prune_labels!
-    JSON.parse @resource["labels/prune"].post
-  end
+  def prune_labels!; post_json("labels/prune") end
 
   def set_labels! thread_id, labels
-    JSON.parse @resource["thread/#{thread_id}/labels"].post :labels => labels.to_a.to_json
+    post_json "thread/#{thread_id}/labels", :labels => labels.to_json
+  end
+
+  def set_state! message_id, state
+    post_json "message/#{message_id}/state", :state => state.to_json
   end
 
 private
+
+  def get_json path, params={}
+    response = @resource[path + ".json"].get :params => params
+    response.force_encoding Encoding::UTF_8 if in_ruby19_hell?
+    JSON.parse response
+  end
+
+  def post_json path, params={ :please => "1" } # you need to have at least one param for RestClient to work... lame
+    response = @resource[path + ".json"].post params
+    response.force_encoding Encoding::UTF_8 if in_ruby19_hell?
+    JSON.parse response
+  end
+
 
   def in_ruby19_hell?
     @in_ruby19_hell = "".respond_to?(:encoding) if @in_ruby19_hell.nil?
