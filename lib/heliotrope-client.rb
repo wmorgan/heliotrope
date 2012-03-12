c379eb3693a61b555dec31facb3d96354607a6f7
William Morgan <wmorgan-github@masanjin.net>
2012-03-12 14:12:03 -0700

    fix up Client#contacts_with_prefix call

--- lib/heliotrope-client.rb
+++ lib/heliotrope-client.rb
@@ -68,7 +68,7 @@ class HeliotropeClient
   def labels; get_json("labels")["labels"] end
   def info; get_json("info") end
   def size; get_json("size")["size"] end
-  def contacts prefix; get_json("contacts")["contacts"] end
+  def contacts_with_prefix prefix, num=100; get_json("contacts", :prefix => prefix, :num => num)["contacts"] end
 
   def prune_labels!; post_json("labels/prune")["labels"] end
 
